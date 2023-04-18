44637b9ef80c67804a951c576f91b8c8a3bcd465
cret1x <cretixthebest@gmail.com>
2023-04-18 19:02:46 +0300

    4 + 5

---
+++ 4/main.c
@@ -0,0 +1,142 @@
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+#include <sys/shm.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+
+// status: 0 - cutter free; 1 - cuttting
+// buf: conatins ids of new client
+// when cutter is cutting he will close sem
+// when free, open it
+// first client to access open sem will change status of 
+// cutter to cutting and close sem
+
+#define CHILDREN_MAX 16
+
+static volatile int keepRunning = 1;
+
+typedef struct shared_memory {
+  int status;
+  int current_client;
+}shared_memory;
+
+
+void intHandler(int dummy) {
+    printf("[Cutter] SIGINT Detected!\n");
+    keepRunning = 0;
+}
+
+void sem_close(int semid) {
+ struct sembuf parent_buf = {
+    .sem_num = 0,
+    .sem_op = -1,
+    .sem_flg = 0
+  };
+  if (semop(semid, & parent_buf, 1) < 0) {
+    printf("Can\'t sub 1 from semaphor\n");
+    exit(-1);
+  }
+}
+
+void sem_open(int semid) {
+ struct sembuf parent_buf = {
+    .sem_num = 0,
+    .sem_op = 1,
+    .sem_flg = 0
+  };
+  if (semop(semid, & parent_buf, 1) < 0) {
+    printf("Can\'t add 1 to semaphor\n");
+    exit(-1);
+  }  
+}
+
+
+void child(int semid, shared_memory* shmptr)  {
+  printf("I am a client #%d!\n", getpid());
+  sleep(rand() % 10 + 2);
+  
+  sem_close(semid);
+  printf("[%d] Going to cutter\n", getpid());
+  shmptr->current_client = getpid();
+  exit(0);
+}
+
+
+
+int main(int argc, char ** argv) {
+  if (argc < 2) {
+    printf("usage: ./main <clients count>");
+    return -1;
+  }
+  signal(SIGINT, intHandler);
+  int clients = atoi(argv[1]);
+  if (clients < 1 || clients > 16) {
+    printf("Invalid clients count!\n");
+    return -1;
+  }
+  key_t key = ftok(argv[0], 0);
+  shared_memory *shared_mem_ptr;
+  int semid;
+  int shmid;
+
+  int children[CHILDREN_MAX];
+  
+  if ((semid = semget(key, 1, 0666 | IPC_CREAT)) < 0) {
+    printf("Can\'t create semaphore\n");
+    return -1;
+  }
+
+  if ((shmid = shmget(key, sizeof(shared_memory), 0666 | IPC_CREAT)) < 0) {
+    printf("Can\'t create shmem\n");
+    return -1;
+  }
+  if ((shared_mem_ptr = (shared_memory*) shmat(shmid, NULL, 0)) == (shared_memory *) -1) {
+    printf("Cant shmat!\n");
+    return -1;
+  } 
+  semctl(semid, 0, SETVAL, 1);
+
+  shared_mem_ptr->status = 0;
+  shared_mem_ptr->current_client = 0;
+
+  // create child processes
+  for (int i = 0; i < clients; i++) {
+    children[i] = fork();
+    if (children[i] == 0) {
+      child(semid, shared_mem_ptr);
+    }
+  }
+
+  printf("I am a Cutter\n");
+  
+  while (keepRunning) {
+    int clid = shared_mem_ptr->current_client;
+    if (clid == 0) {
+      printf("[Cutter] No new clients\n");
+      sleep(2);
+    } else {
+      printf("[Cutter] new client #%d\n", clid);
+      sleep(5);
+      printf("[Cutter] finished client #%d\n", clid);
+      shared_mem_ptr->current_client = 0;
+      sem_open(semid);
+    }
+
+  }
+  for (int i = 0; i < clients; i++) {
+    kill(children[i], SIGTERM);
+    printf("[Cutter] Killing child #%d\n", children[i]);
+  }
+
+  if (semctl(semid, 0, IPC_RMID, 0) < 0) {
+	printf("Can\'t delete semaphore\n");
+  	return -1;
+  }
+  shmdt(shared_mem_ptr);
+  shmctl(shmid, IPC_RMID, NULL);
+  return 0;
+}
---
+++ 5/main.c
@@ -0,0 +1,130 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <semaphore.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+#include <string.h>
+#include <time.h>
+
+// status: 0 - cutter free; 1 - cuttting
+// buf: conatins ids of new client
+// when cutter is cutting he will close sem
+// when free, open it
+// first client to access open sem will change status of 
+// cutter to cutting and close sem
+
+#define CHILDREN_MAX 16
+
+static volatile int keepRunning = 1;
+
+typedef struct shared_memory {
+  sem_t mutex;
+  int current_client;
+}shared_memory;
+
+
+void intHandler(int dummy) {
+    printf("[Cutter] SIGINT Detected!\n");
+    keepRunning = 0;
+}
+
+
+void child(shared_memory *shmem)  {
+  printf("I am a client #%d!\n", getpid());
+  srand(time(0));
+  sleep(rand() % 10 + 2);
+  
+  sem_wait(&shmem->mutex);
+  printf("[%d] Going to cutter\n", getpid());
+  shmem->current_client = getpid();
+  exit(0);
+}
+
+
+
+int main(int argc, char ** argv) {
+  if (argc < 2) {
+    printf("usage: ./main <clients count>");
+    return -1;
+  }
+  signal(SIGINT, intHandler);
+  int clients = atoi(argv[1]);
+  if (clients < 1 || clients > CHILDREN_MAX) {
+    printf("Invalid clients count!\n");
+    return -1;
+  }
+  int children[CHILDREN_MAX];
+  char memn[] = "shared-memory"; //  имя объекта
+  int mem_size = sizeof(shared_memory);
+  int shm;
+  sem_t mutex;
+
+  // СОздать память
+  if ((shm = shm_open(memn, O_CREAT | O_RDWR, 0666)) == -1) {
+      printf("Object is already open\n");
+      perror("shm_open");
+      return 1;
+  } else {
+      printf("Object is open: name = %s, id = 0x%x\n", memn, shm);
+  }
+  if (ftruncate(shm, mem_size) == -1) {
+      printf("Memory sizing error\n");
+      perror("ftruncate");
+      return 1;
+  } else {
+      printf("Memory size set and = %d\n", mem_size);
+  }
+
+  //получить доступ к памяти
+  void* addr = mmap(0, mem_size, PROT_WRITE, MAP_SHARED, shm, 0);
+  if (addr == (int * ) - 1) {
+      printf("Error getting pointer to shared memory\n");
+      return 1;
+  }
+
+  shared_memory* shmem = addr;
+  sem_init(&shmem->mutex, 1, 1);
+  shmem->current_client = 0;
+
+
+  // create child processes
+  for (int i = 0; i < clients; i++) {
+    children[i] = fork();
+    if (children[i] == 0) {
+      child(shmem);
+    }
+  }
+
+  printf("I am a Cutter\n");
+  
+  while (keepRunning) {
+    int clid = shmem->current_client;
+    if (clid == 0) {
+      printf("[Cutter] No new clients\n");
+      sleep(2);
+    } else {
+      printf("[Cutter] new client #%d\n", clid);
+      sleep(5);
+      printf("[Cutter] finished client #%d\n", clid);
+      shmem->current_client = 0;
+      sem_post(&shmem->mutex);
+    }
+
+  }
+  for (int i = 0; i < clients; i++) {
+    kill(children[i], SIGTERM);
+    printf("[Cutter] Killing child #%d\n", children[i]);
+  }
+
+  sem_destroy(&shmem->mutex);
+  close(shm);
+  // удалить выделенную память
+  if(shm_unlink(memn) == -1) {
+    printf("Shared memory is absent\n");
+    perror("shm_unlink");
+    return 1;
+  }
+  return 0;
+}
